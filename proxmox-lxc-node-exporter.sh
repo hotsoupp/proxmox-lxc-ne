d7b91d762e5526f50dcc8523493355e509cdfa10
Alex ten Cate <80267522+hotsoupp@users.noreply.github.com>
2022-02-18 16:06:40 +0100

    Add files via upload

---
+++ proxmox-lxc-node-exporter.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Install Node Export on all LXC's
+
+# list of container ids we need to iterate through
+containers=$(pct list | tail -n +2 | cut -f1 -d' ')
+
+function update_container() {
+  container=$1
+  echo "Installing Node Exporter on $container..."
+  # to chain commands within one exec we will need to wrap them in bash
+  pct exec $container -- bash -c "wget https://raw.githubusercontent.com/hotsoupp/node-exporter/main/node_exporter.sh && chmod +x node_exporter.sh && ./node_exporter.sh"
+}
+
+for container in $containers
+do
+  update_container $container
+done
